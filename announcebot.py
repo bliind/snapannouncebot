fc256cc8a229948c65845ac79d21301d436879b4
Mike Sweeney <blindx@gmail.com>
2023-08-14 22:03:23 -0400

    Add right-click command for posting

--- announcebot.py
+++ announcebot.py
@@ -2,7 +2,39 @@ import discord
 import json
 import os
 import re
-from discord.ext import tasks
+from discord import app_commands
+
+class PostModal(discord.ui.Modal, title='Post Where?'):
+    def __init__(self, the_message):
+        super().__init__()
+        self.the_message = the_message
+
+    channel = discord.ui.TextInput(label='Channel')
+    async def on_submit(self, interaction: discord.Interaction):
+        message = self.the_message
+
+        guild = [g for g in bot.guilds if g.id == config.server][0]
+        channel = discord.utils.get(guild.channels, name=self.channel.value.replace('#', ''))
+
+        if channel:
+            files = []
+            for file in message.attachments:
+                files.append(await file.to_file())
+
+            if channel.type == discord.ChannelType.forum:
+                title = message.content.splitlines()[0]
+                actual_content = message.content.replace(title, '').strip()
+                thread = await channel.create_thread(name=title, files=files, content=actual_content)
+                if thread:
+                    await interaction.response.send_message(thread.message.jump_url, ephemeral=True)
+                    await message.add_reaction("✅")
+            else:
+                sent = await channel.send(message.content, files=files)
+                if sent:
+                    await interaction.response.send_message(sent.jump_url, ephemeral=True)
+                    await message.add_reaction("✅")
+        else:
+            await interaction.response.send_message('Failed, sorry', ephemeral=True)
 
 class dotdict(dict):
     """dot.notation access to dictionary attributes"""
@@ -23,9 +55,11 @@ load_config()
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
+tree = app_commands.CommandTree(bot)
 
 @bot.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=config.server))
     print(f"{config.env.upper()} ANNOUNCEBOT is ready for duty")
 
 @bot.event
@@ -57,5 +91,10 @@ async def on_message(message):
                         await message.add_reaction("✅")
             else:
                 await message.channel.send('No channel specified')
+
+@tree.context_menu(name='Post Announcement', guild=discord.Object(id=config.server))
+async def dev_reply_command(interaction, message: discord.Message):
+    modal = PostModal(message)
+    await interaction.response.send_modal(modal)
 
 bot.run(config.token)
